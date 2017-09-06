8d3093c2daf50ca00c891a20edb9debf72fa58b2
jk <kineticsquid@gmail.com>
2017-09-06 16:36:16 -0400

    One of many commits to get Bkluemix DevOps pipeline working

--- Miscellaneous/test.sh
+++ Miscellaneous/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "hello, $USER. testing now..."
 
-wsk action invoke return_environment --blocking --result --param name Fred
+bx wsk action invoke return_environment --blocking --result --param name Fred
 echo ""
 
-wsk action invoke return_environment --blocking --result
+bx wsk action invoke return_environment --blocking --result
 echo ""
 
 
