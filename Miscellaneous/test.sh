c0a49fdf6da6993d7ef15ae98872899447c3702f
jk <kineticsquid@gmail.com>
2017-09-07 15:17:10 -0400

    One of many commits to get Bkluemix DevOps pipeline working

--- Miscellaneous/test.sh
+++ Miscellaneous/test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo "hello, $USER. testing now..."
 
+# Now update the action at Bluemix, also creates the action if it doesn't exist
+bx api https://api.ng.bluemix.net
+bx plugin install Cloud-Functions -r Bluemix
+# bx login --apikey ${WSK_AUTH}
+# bx target -o ${CF_ORG} -s ${CF_SPACE}
+# bx wsk action update return_environment ./Miscellaneous.zip --kind python:3
+
 bx wsk action invoke return_environment --blocking --result --param name Fred
 echo ""
 
